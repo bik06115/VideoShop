4b085c9ded049522bd59c5600e98d3ba8655e143
bik06115 <67669686+bik06115@users.noreply.github.com>
2020-08-26 00:46:23 +1200

    Add files via upload

---
+++ DBConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VideoRentShop;
+
+namespace VideoShopUnitTests
+{
+    [TestClass]
+    public class Test_Connection_DB
+    {
+        [TestMethod]
+        public void Test_Connection_DB_OpenSate_ReturnsTrue()
+        {
+            VideoShop ms = new VideoShop();
+            bool v = ms.TestConnectionDatabase();
+            Assert.IsTrue(v);
+        }
+    }
+}
---
+++ VideoShopTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VideoRentShop;
+
+namespace VideoShopUnitTests
+{
+    [TestClass]
+    public class VideoShopTests
+    {
+        [TestMethod]
+        public void Get_RentedMovies_Data_ReturnsDataObject()
+        {
+            VideoShop ms = new VideoShop();
+            DataTable d = ms.ListRentedMovies();
+            Assert.IsNotNull(d);
+        }
+    }
+}
